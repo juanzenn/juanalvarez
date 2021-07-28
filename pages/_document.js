4d97c31333899bc5d21bc37ac1941db42d1e564e
Juan Alvarez <juanandres140299@gmail.com>
2021-07-28 13:39:20 -0400

    Visitor analytics test 3

--- pages/_document.js
+++ pages/_document.js
@@ -14,9 +14,10 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
           <script
+            async
             dangerouslySetInnerHTML={{
               __html: `
-            (function(v,i,s,a){if(!v._visaSettings){v._visaSettings={};}v._visaSettings["7d19bd28-efc8-11eb-b589-901b0edac50a"]={v:"0.3",s:"7d19bd28-efc8-11eb-b589-901b0edac50a",a:"1"};_v=i.getElementsByTagName("body")[0];_a=_v;_i=i.createElement("script");_s=_i;_s.defer="defer";_s.src=s+a+v._visaSettings["7d19bd28-efc8-11eb-b589-901b0edac50a"].v;_a.appendChild(_s);})(window,document,"//app-worker.visitor-analytics.io/main",".js?s=7d19bd28-efc8-11eb-b589-901b0edac50a&v=")<!-- VISA Tracking Code for https://juanalvarez.vercel.app/ -->
+            (function(v,i,s,a){if(!v._visaSettings){v._visaSettings={};}v._visaSettings["7d19bd28-efc8-11eb-b589-901b0edac50a"]={v:"0.3",s:"7d19bd28-efc8-11eb-b589-901b0edac50a",a:"1"};_v=i.getElementsByTagName("body")[0];_a=_v;_i=i.createElement("script");_s=_i;_s.defer="defer";_s.src=s+a+v._visaSettings["7d19bd28-efc8-11eb-b589-901b0edac50a"].v;_a.appendChild(_s);})(window,document,"//app-worker.visitor-analytics.io/main",".js?s=7d19bd28-efc8-11eb-b589-901b0edac50a&v=")
             `,
             }}
           />
