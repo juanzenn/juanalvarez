43eac812ca38b1354824b12360c54aaed1ea24b6
Juan Alvarez <juanandres140299@gmail.com>
2021-07-29 09:23:00 -0400

    Added image for the covers

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home({ slices }) {
           property='og:description'
           content="I make websites and web applications for modern businesses. If you want to have a strong online presence, contact me. Let's work together."
         />
-        <meta property='og:image' content='' />
+        <meta property='og:image' content='/cover.png' />
 
         <meta property='twitter:card' content='summary_large_image' />
         <meta
@@ -41,7 +41,7 @@ export default function Home({ slices }) {
           property='twitter:description'
           content="I make websites and web applications for modern businesses. If you want to have a strong online presence, contact me. Let's work together."
         />
-        <meta property='twitter:image' content='' />
+        <meta property='twitter:image' content='/cover.png' />
         <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
         <link rel='icon' href='/favicon.ico' type='image/x-icon' />
       </Head>
