f6c0905dbadb515c82037210cf35545e69ff6e48
Juan Alvarez <juanandres140299@gmail.com>
2021-07-29 09:15:58 -0400

    SEO tags

--- pages/index.js
+++ pages/index.js
@@ -9,8 +9,39 @@ export default function Home({ slices }) {
   return (
     <>
       <Head>
-        <title>Juan Alvarez</title>
-        <link rel='icon' href='/favicon.ico' />
+        <title>Juan Alvarez | Front-end web developer</title>
+        <meta name='title' content='Juan Alvarez | Front-end web developer' />
+        <meta
+          name='description'
+          content="I make websites and web applications for modern businesses. If you want to have a strong online presence, contact me. Let's work together."
+        />
+
+        <meta property='og:type' content='website' />
+        <meta property='og:url' content='https://juanalvarez.vercel.app/' />
+        <meta
+          property='og:title'
+          content='Juan Alvarez | Front-end web developer'
+        />
+        <meta
+          property='og:description'
+          content="I make websites and web applications for modern businesses. If you want to have a strong online presence, contact me. Let's work together."
+        />
+        <meta property='og:image' content='' />
+
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta
+          property='twitter:url'
+          content='https://juanalvarez.vercel.app/'
+        />
+        <meta
+          property='twitter:title'
+          content='Juan Alvarez | Front-end web developer'
+        />
+        <meta
+          property='twitter:description'
+          content="I make websites and web applications for modern businesses. If you want to have a strong online presence, contact me. Let's work together."
+        />
+        <meta property='twitter:image' content='' />
       </Head>
       <Navbar />
 
