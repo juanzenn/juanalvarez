16d44f26624bf9baacb47a3867cc174e7c8f3dfe
Juan Alvarez <juanandres140299@gmail.com>
2023-02-12 19:29:13 -0400

    change theme-color

--- pages/_document.js
+++ pages/_document.js
@@ -17,6 +17,7 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <meta name="theme-color" content="#000" />
         </Head>
 
         <body>
