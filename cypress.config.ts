9bb10168c5d19d746c2e6fdeeaf30cfe064b9f83
Juan Alvarez <31358977+juanzenn@users.noreply.github.com>
2023-02-12 18:47:06 -0400

    update cypress yaml (#3)
    * better cypres yaml
    
    * modify build folder
    
    * fix cypress build name
    
    * fix name and paths
    
    * change container to node 18
    
    * my brain doesnt work
    
    * save node on cypress cache
    
    * download node modules
    
    * test with install
    
    * remove record
    
    * cypress app id
    
    * oopsi
    
    * add same container to install
    
    * ?
    
    * with failing test
    
    * lets try one more time
    
    * one-job does it all
    
    * copy somethings from RWA
    
    * delete de matrix from install
    
    * remove record from install
    
    * fix multiple instances "install" error and remove falsy test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "cypress";
 
 module.exports = defineConfig({
+  projectId: "1fxcd6",
   e2e: {
     baseUrl: "http://localhost:3000",
+    projectId: "1fxcd6",
   },
 });
