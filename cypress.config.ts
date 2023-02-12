555668e461fcc665bff4f207664cd027b3db7d4d
Juan Alvarez <juanandres140299@gmail.com>
2023-02-12 18:21:35 -0400

    one-job does it all

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "cypress";
 module.exports = defineConfig({
   projectId: "1fxcd6",
   e2e: {
-    projectId: "1fxcd6",
     baseUrl: "http://localhost:3000",
+    projectId: "1fxcd6",
   },
 });
