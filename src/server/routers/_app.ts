a00f9a0134a02867535cf9c7561eb351a09ce9f9
Juan Alvarez <juanandres140299@gmail.com>
2023-06-04 20:42:03 -0400

    chore: remove old pages folder and unused deps

--- src/server/routers/_app.ts
+++
@@ -1,9 +0,0 @@
-import { router } from "../trpc";
-import { mailRouter } from "./mail";
-
-export const appRouter = router({
-  mail: mailRouter,
-});
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
